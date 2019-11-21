a60674d93e88c47dd8ba0986fdcd272cb9508a5b
zhy <38369016+tomorrow1210@users.noreply.github.com>
2019-11-21 11:17:46 +0800

    Add files via upload
    cvpr_paper:you can climb cvpr paper through this code from http://openaccess.thecvf.com/,while iccv and eccv with minor edits.

---
+++ cvpr_paper.py
@@ -0,0 +1,36 @@
+# coding:utf-8
+import re
+import requests
+import urllib
+import os
+# get web context
+year = '2019'
+r = requests.get('http://openaccess.thecvf.com/CVPR{}.py'.format(year))
+data = r.text
+# find all pdf links
+link_list = re.findall(r"(?<=href=\").+?pdf(?=\">pdf)|(?<=href=\').+?pdf(?=\">pdf)", data)
+name_list = re.findall(r"(?<=href=\").+?{}_paper.html\">.+?</a>".format(year), data)
+cnt = 0
+num = len(link_list)
+# your local path to download pdf files
+localDir = '/home/admin/cvpr2019/'
+if not os.path.exists(localDir):
+    os.makedirs(localDir)
+while cnt < num:
+    url = link_list[cnt]
+    # seperate file name from url links
+    file_name = name_list[cnt].split('<')[0].split('>')[1]
+    # to avoid some illegal punctuation in file name
+    file_name = file_name.replace(':','_')
+    file_name = file_name.replace('\"','_')
+    file_name = file_name.replace('?','_')
+    file_name = file_name.replace('/','_')
+    file_path = localDir + file_name + '.pdf'
+    # download pdf files
+    print('['+str(cnt)+'/'+str(num)+"]  Downloading -> "+file_path)
+    try:
+        urllib.request.urlretrieve('http://openaccess.thecvf.com/'+url, file_path)
+    except:
+        continue
+    cnt = cnt + 1
+print("all download finished")
